50bd336fcda29727504ae1bc6ed061f964ee90b7
Sebastian Carlson <sebcar98@gmail.com>
2020-09-30 10:30:40 +0200

    added 21 more checkboxes for question

--- quiz.js
+++ quiz.js
@@ -4,10 +4,10 @@
 
 function fetchData() {
 
-    fetch('https://quizapi.io/api/v1/questions?apiKey=ApIq7xjNwiSVvRYkKtGckbRBzQvw1MhiHbM1iZFe&limit=10')
+    fetch('https://quizapi.io/api/v1/questions?apiKey=ApIq7xjNwiSVvRYkKtGckbRBzQvw1MhiHbM1iZFe&limit=10') // + [i] för att välja antal frågor
     .then(response => response.json())
     .then(data => {
-        console.log(data[0]);
+        console.log(data);
     });
 }
 
