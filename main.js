6476e8526e0b8cc30e987ad56af6e70ac6263d73
Sebastian Carlson <sebcar98@gmail.com>
2020-10-01 11:34:36 +0200

    jobbar på att spara ner alla frågot i en array

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function (e) {    
-    player_button = document.getElementById("playerBtn");
-    game_button = document.getElementById("gameBtn");
-    
+    let player_button = document.getElementById("playerBtn");
+    let player_info = document.getElementById("info-player");
+    let player_score = document.getElementById("score-player");
+    let quiz = new Quiz();
+
+    console.log(quiz.question);
+
     player_button.addEventListener("click", function (e) {
         let name = document.getElementById("player-text").value;
         let score = 0;
         let player = new Player(name, score);
         console.log(player);
+        player_info.innerHTML = "Name: " + player.name;
+        player_score.innerHTML = "Score: " + player.score;
     })
     
+
 
 });
--- quiz.js
+++ quiz.js
@@ -1,15 +1,18 @@
 class Quiz{
     constructor(){
-
-}
-
+        this.question = this.fetchData;
+    }
 fetchData() {
-
     fetch('https://quizapi.io/api/v1/questions?apiKey=ApIq7xjNwiSVvRYkKtGckbRBzQvw1MhiHbM1iZFe&limit=10') // + [i] för att välja antal frågor
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        
+        for(let questionArray of data) {
+            questionArray.push(data);
+        }
     });
+
+    
 }
 }
 
