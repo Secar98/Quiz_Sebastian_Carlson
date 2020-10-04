2e156c17dc1c9b2be280a5b2fe0bf1347d232b8b
Sebastian Carlson <sebcar98@gmail.com>
2020-10-04 18:57:29 +0200

    skapat en count för att itterera genom frågorna

--- game.js
+++ game.js
@@ -11,16 +11,25 @@ class Game {
         let answerInput4 = document.getElementById("text-input4");
         let answerInput5 = document.getElementById("text-input5");
         let answerInput6 = document.getElementById("text-input6");
-        
-        question.innerHTML = myArray[0].question;
-        answerInput1.innerHTML = myArray[0].answers.answer_a;
-        answerInput2.innerHTML = myArray[0].answers.answer_b;
-        answerInput3.innerHTML = myArray[0].answers.answer_c;
-        answerInput4.innerHTML = myArray[0].answers.answer_d;
-        answerInput5.innerHTML = myArray[0].answers.answer_e;
-        answerInput6.innerHTML = myArray[0].answers.answer_f;
 
-        console.log(myArray[0]);
+        
+
+        let submit = document.getElementById("submit");
+
+        let count = 0;
+        
+        submit.addEventListener("click", function (e) {
+                question.innerHTML = myArray[count].question;
+                answerInput1.innerHTML = myArray[count].answers.answer_a;
+                answerInput2.innerHTML = myArray[count].answers.answer_b;
+                answerInput3.innerHTML = myArray[count].answers.answer_c;
+                answerInput4.innerHTML = myArray[count].answers.answer_d;
+                answerInput5.innerHTML = myArray[count].answers.answer_e;
+                answerInput6.innerHTML = myArray[count].answers.answer_f;
+
+                
+                count++;
+        })
         
     }
 }
@@ -28,9 +37,6 @@ class Game {
 
 
 /*
-Behöver skriva ut question till innerHTML *
-Behöver skriva ut alla answers mha "answers" till innerHTML *
-Behöver hämta "correct_answer" och gemföra med vad användaren har valt
-
-Kanske behöver hämta "multiple_correct_answers" och jämföra med correct answer
+    Behöver hämta vad användaren klickar i för checkbox,
+    och sen jämföra det med correct_answers inte correct_answer
 */
