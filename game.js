df2b4798dadd03e30dcc05519c97338d67e254c4
Sebastian Carlson <sebcar98@gmail.com>
2020-10-05 09:03:02 +0200

    lag till en starta om funktion i game classen

--- game.js
+++ game.js
@@ -12,23 +12,31 @@ class Game {
         let answerInput5 = document.getElementById("text-input5");
         let answerInput6 = document.getElementById("text-input6");
 
-        
-
         let submit = document.getElementById("submit");
 
         let count = 0;
         
+        // 
         submit.addEventListener("click", function (e) {
+            if (count < 10) { // ittererar genom frågorna    
+                console.log(count);
+                submit.textContent = "Nästa fråga!"
                 question.innerHTML = myArray[count].question;
-                answerInput1.innerHTML = myArray[count].answers.answer_a;
-                answerInput2.innerHTML = myArray[count].answers.answer_b;
-                answerInput3.innerHTML = myArray[count].answers.answer_c;
-                answerInput4.innerHTML = myArray[count].answers.answer_d;
-                answerInput5.innerHTML = myArray[count].answers.answer_e;
-                answerInput6.innerHTML = myArray[count].answers.answer_f;
-
-                
+                answerInput1.textContent = myArray[count].answers.answer_a;
+                answerInput2.textContent = myArray[count].answers.answer_b;
+                answerInput3.textContent = myArray[count].answers.answer_c;
+                answerInput4.textContent = myArray[count].answers.answer_d;
+                answerInput5.textContent = myArray[count].answers.answer_e;
+                answerInput6.textContent = myArray[count].answers.answer_f;
                 count++;
+            } else if (count == 10) { // starta om spelet
+                submit.textContent = "Tryck för att starta om!";
+                count = 0;
+                let new_quiz = new Quiz(10);
+                new_quiz.fetchApi();
+                console.log(count);
+            }                                 
+
         })
         
     }
